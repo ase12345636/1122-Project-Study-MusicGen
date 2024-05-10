f2173c23f092845dbeebc1b8806fa327a13348a9
YiYuan <yiyuan0918@gmail.com>
2024-05-11 00:50:42 +0800

    Add test transformer structure (In __MusicGen)

---
+++ __MusicGen/Transformer.py
@@ -0,0 +1,173 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import copy
+
+import torch
+from torch import nn, Tensor
+from torch.nn import TransformerEncoder, TransformerEncoderLayer, TransformerDecoder, TransformerDecoderLayer
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, ntoken: int, d_model: int, nhead: int, d_hid: int,
+                 nlayer: int, dropout: float = 0.5):
+        super().__init__()
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = TransformerEncoderLayer(
+            d_model, nhead, d_hid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayer)
+        self.embedding = nn.Embedding(ntoken, d_model)
+        self.d_model = d_model
+
+        self.init_weights()
+
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src: Tensor) -> Tensor:
+        """
+        Arguments:
+            src: Tensor, shape ``[seq_len, batch_size]``
+            src_mask: Tensor, shape ``[seq_len, seq_len]``
+
+        Returns:
+            output Tensor of shape ``[seq_len, batch_size, ntoken]``
+        """
+        src = self.embedding(src) * math.sqrt(self.d_model)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src)
+        return output
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, ntoken: int, d_model: int, nhead: int, d_hid: int,
+                 nlayer: int, dropout: float = 0.5):
+        super().__init__()
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        decoder_layers = TransformerDecoderLayer(
+            d_model, nhead, d_hid, dropout)
+        self.transformer_decoder = TransformerDecoder(decoder_layers, nlayer)
+        self.embedding = nn.Embedding(ntoken, d_model)
+        self.d_model = d_model
+        self.linear = nn.Linear(d_model, ntoken)
+
+        self.init_weights()
+
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.linear.bias.data.zero_()
+        self.linear.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src: Tensor, src_mask: Tensor = None) -> Tensor:
+        """
+        Arguments:
+            src: Tensor, shape ``[seq_len, batch_size]``
+            src_mask: Tensor, shape ``[seq_len, seq_len]``
+
+        Returns:
+            output Tensor of shape ``[seq_len, batch_size, ntoken]``
+        """
+        src = self.embedding(src) * math.sqrt(self.d_model)
+        src = self.pos_encoder(src)
+        if src_mask is None:
+            """Generate a square causal mask for the sequence. The masked positions are filled with float('-inf').
+            Unmasked positions are filled with float(0.0).
+            """
+            src_mask = nn.Transformer.generate_square_subsequent_mask(
+                len(src)).to('cuda')
+        output = self.transformer_decoder(src, src_mask)
+        output = self.linear(output)
+        return output
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2)
+                             * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+
+class TransformerModel(nn.Module):
+    def __init__(self, ntoken: int, d_model: int, nhead: int, nlayer: int, d_hid: int, dropout: int):
+        super(TransformerModel, self).__init__()
+
+        self.encoder = Encoder(
+            ntoken=ntoken, d_model=d_model, nhead=nhead, d_hid=d_hid, nlayer=nlayer, dropout=dropout)
+        self.decoder = Decoder(
+            ntoken=ntoken, d_model=d_model, nhead=nhead, d_hid=d_hid, nlayer=nlayer, dropout=dropout)
+
+    def forward(self, src, tgt):
+        output = self.decoder(tgt, self.encoder(src))
+        return output
+
+
+ntoken = 5000
+d_model = 512
+nheads = 8
+nlayer = 6
+d_hid = 2048
+dropout = 0.1
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+transformer = TransformerModel(
+    ntoken, d_model, nheads, nlayer, d_hid, dropout)
+transformer.to(device)
+
+# Generate random sample data
+# (batch_size, seq_length)
+src_data = torch.randint(1, ntoken, (64, 100)).to(device)
+# (batch_size, seq_length)
+tgt_data = torch.randint(1, ntoken, (64, 100)).to(device)
+
+criterion = nn.CrossEntropyLoss(ignore_index=0)
+optimizer = optim.Adam(transformer.parameters(),
+                       lr=0.0001, betas=(0.9, 0.98), eps=1e-9)
+
+transformer.train()
+
+for epoch in range(10):
+    optimizer.zero_grad()
+    output = transformer(src_data, tgt_data)
+    loss = criterion(output.contiguous().view(-1, ntoken),
+                     tgt_data.contiguous().view(-1))
+    loss.backward()
+    optimizer.step()
+    print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+
+transformer.eval()
+
+# Generate random sample validation data
+# (batch_size, seq_length)
+val_src_data = torch.randint(1, ntoken, (64, 100)).to(device)
+# (batch_size, seq_length)
+val_tgt_data = torch.randint(1, ntoken, (64, 100)).to(device)
+
+with torch.no_grad():
+
+    val_output = transformer(val_src_data, val_tgt_data)
+    val_loss = criterion(val_output.contiguous(
+    ).view(-1, ntoken), val_tgt_data.contiguous().view(-1))
+    print(f"Validation Loss: {val_loss.item()}")
